886bcf129ab007a18016ef92425b81c6190b7936
rozran00@gmail.com <rozran00@gmail.com>
2021-01-22 11:43:56 -0500

    updated wiht SRS example

---
+++ STAT1313_Spring2021/Simple Random Sample Example.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+
+
+df <- data.frame()
+for(i in 1:100){
+    samp <- sample(1:10, 4)
+    tmp <- data.frame(iteration = i,
+                      mean = mean(samp),
+                      median = median(samp),
+                      samp = paste(samp, collapse = ","))
+    
+    df <- rbind(df, tmp)
+}
+
+ggplot(data = df, aes(x = iteration, y = mean)) +
+    geom_point() +
+    # geom_line() +
+    geom_hline(yintercept = mean(1:10), color = "red") + 
+    ggtitle(paste(dim(df)[1], "four 'person' samples from 1-10; average of the",
+                  " averages is: ", mean(df$mean)))
