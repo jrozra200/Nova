2a9a4e1ba931bd06cc7e21efa68ee89343fd7255
rozran00@gmail.com <rozran00@gmail.com>
2021-03-14 11:58:44 -0400

    chapter 9 viz updates

--- STAT1313_Spring2021/Chapter 9 Viz.R
+++ STAT1313_Spring2021/Chapter 9 Viz.R
@@ -36,3 +36,23 @@ ggplot(other_dat) +
           legend.title = element_blank(), panel.background = element_blank(),
           axis.ticks = element_blank(), axis.title.y = element_blank())
 table(other_dat$Sex)
+
+# Section 4
+
+dat <- data.frame(eaten = c("Eaten", "Not Eaten"),
+                  infection = c("Uninfected", "Uninfected", "Lightly Infected",
+                               "Lightly Infected", "Highly Infected", 
+                               "Highly Infected"),
+                  values = c(1, 49, 10, 35, 37, 9))
+
+dat$eaten <- factor(dat$eaten, levels = c("Not Eaten", "Eaten"))
+dat$infection <- factor(dat$infection, 
+                        levels = c("Uninfected", "Lightly Infected", 
+                                   "Highly Infected"))
+
+ggplot(dat) + 
+    geom_mosaic(aes(x = product(eaten, infection), fill = eaten, weight = values)) + 
+    scale_fill_brewer(palette = "Set1") + 
+    theme(text = element_text(size = 16), legend.position = "top", 
+          legend.title = element_blank(), panel.background = element_blank(),
+          axis.ticks = element_blank(), axis.title = element_blank())
