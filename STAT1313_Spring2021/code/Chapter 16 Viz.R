0a7444a59f41a3d2ea35ac2a88fc4fe1c38e5edd
rozran00@gmail.com <rozran00@gmail.com>
2021-04-19 20:25:03 -0400

    finished chapter 16

--- STAT1313_Spring2021/code/Chapter 16 Viz.R
+++ STAT1313_Spring2021/code/Chapter 16 Viz.R
@@ -81,3 +81,45 @@ ggplot(plot_dat[plot_dat$x >= -1 & plot_dat$x <= 0, ], aes(x = x, y = y)) +
                                                     plot_dat$x <= 0], 
                                      plot_dat$y[plot_dat$x >= -1 & 
                                                     plot_dat$x <= 0]), 5)))
+
+plot_dat <- data.frame(x = sort(rnorm(40), decreasing = TRUE),
+                       y = sort(rnorm(40), decreasing = TRUE))
+
+plot_dat$x_var <- plot_dat$x + rnorm(40, 1, 0.5)
+plot_dat$y_var <- plot_dat$y + rnorm(40, 1, 0.5)
+
+library(ggplot2)
+
+plot1 <- ggplot(plot_dat, aes(x = x, y = y)) + 
+    geom_point() + 
+    theme(panel.background = element_blank(), 
+          panel.grid.major.y = element_line(color = "light gray"),
+          panel.grid.major.x = element_line(color = "light gray"),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_line(color = "light gray"),
+          text = element_text(size = 20)) + 
+    ggtitle(paste0("r = ", round(cor(plot_dat$x, plot_dat$y), 5)))
+
+plot2 <- ggplot(plot_dat, aes(x = x, y = y_var)) + 
+    geom_point() + 
+    theme(panel.background = element_blank(), 
+          panel.grid.major.y = element_line(color = "light gray"),
+          panel.grid.major.x = element_line(color = "light gray"),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_line(color = "light gray"),
+          text = element_text(size = 20)) + 
+    ggtitle(paste0("r = ", round(cor(plot_dat$x, plot_dat$y_var), 5)))
+
+plot3 <- ggplot(plot_dat, aes(x = x_var, y = y_var)) + 
+    geom_point() + 
+    theme(panel.background = element_blank(), 
+          panel.grid.major.y = element_line(color = "light gray"),
+          panel.grid.major.x = element_line(color = "light gray"),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_line(color = "light gray"),
+          text = element_text(size = 20)) + 
+    ggtitle(paste0("r = ", round(cor(plot_dat$x_var, plot_dat$y_var), 5)))
+
+library(gridExtra)
+
+grid.arrange(plot1, plot2, plot3, nrow = 1)
