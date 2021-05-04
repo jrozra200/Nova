9e0bfcdea3d0a448b97b3a662dc2838443dee5a6
rozran00@gmail.com <rozran00@gmail.com>
2021-05-04 08:49:47 -0400

    more graphs

--- STAT1313_Spring2021/code/final_code.R
+++ STAT1313_Spring2021/code/final_code.R
@@ -1,5 +1,7 @@
 # Test example
 
+library(dplyr)
+
 set.seed(317)
 dat <- sample_n(cars, 20)
 dat$x_minus_mean <- dat$speed - mean(dat$speed)
@@ -12,10 +14,23 @@ library(ggplot2)
 
 ggplot(dat, aes(x = speed, y = dist)) + 
     geom_point() + 
+    theme(panel.background = element_blank(), 
+          panel.grid.major.y = element_line(color = "light gray"),
+          panel.grid.major.x = element_blank(),
+          axis.ticks = element_blank())
     geom_smooth(method = 'lm', formula = y ~ x, se = FALSE)
 
 cor(dat$speed, dat$dist)
 
-lm <- glm(dist ~ speed, data = dat, formula = y ~ x)
+linmod <- lm(dist ~ speed, data = dat)
 
-ggplot(lm$residuals)
+lmod <- data.frame(x = 1:20, 
+                   residuals = linmod$residuals)
+
+ggplot(lmod, aes(x = x, y = residuals)) + 
+    geom_point() + 
+    geom_hline(yintercept = 0, color = "red") + 
+    theme(panel.background = element_blank(), 
+          panel.grid.major.y = element_line(color = "light gray"),
+          panel.grid.major.x = element_blank(),
+          axis.ticks = element_blank())
